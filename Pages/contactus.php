318f417a020ac831498ef181e5a0b68812a638da
Abdulrahman Ali <abdalrahman.ali6666@gmail.com>
2023-07-12 15:56:43 +0300

    added pdf and checkbox to contact form in contact page

--- Pages/contactus.php
+++ Pages/contactus.php
@@ -19,6 +19,18 @@
                 <div><input type="input" placeholder="<?=__('phone')?>" id="phone" /></div>
                 <div><input type="input" placeholder="<?=__('email')?>" id="email" /></div>
                 <div><textarea type="input" placeholder="<?=__('message')?>" id="message"></textarea></div>
+                <label>
+                    <input type="checkbox" id="kvkk" /> <small> <?=__('kvkk')?> </small>
+                    <a href="<?=APP_FOLDER?>/file/KVKK Aydınlatma Metni.pdf" target="_blank">
+                        <img src="img/Icon_Link_black.svg" class="h22px" title="Icon_Link" alt="Icon_Link"/>
+                    </a>
+                </label>
+                <label>
+                    <input type="checkbox" id="digital_sign" /> <small> <?=__('digital_sign')?> </small>
+                    <a href="<?=APP_FOLDER?>/file/Elektronik İleti Gönderimini Kabul Ediyorum.pdf" target="_blank">
+                        <img src="img/Icon_Link_black.svg" class="h22px" title="Icon_Link" alt="Icon_Link"/>
+                    </a>
+                </label>
                 <div><button class="butn butn-red"><?=__('contact')?></button></div>
             </form>
 
